d830c14c1706e6e06c8c51d24f1c364bda3125ed
HolanArdinata <113578756+HolanArdinata@users.noreply.github.com>
2023-03-22 22:03:27 +0800

    Add files via upload

---
+++ posttest3/src/Alat.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS-GK
+ */
+
+public class Alat extends AlatOlahraga {
+    protected String tipe;
+    protected String merk;
+
+    public String getTipe() {
+        return tipe;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setTipe(String tipe) {
+        this.tipe = tipe;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public Alat(String nama, String tipe, String merk, String stok, int harga) {
+        super(nama, stok, harga);
+        this.tipe = tipe;
+        this.merk = merk;
+    }
+
+}
---
+++ posttest3/src/Sepatu.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS-GK
+ */
+public class Sepatu extends AlatOlahraga{
+    protected String ukuran;
+    protected String merk;
+
+    public void setUkuran(String ukuran) {
+        this.ukuran = ukuran;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getUkuran() {
+        return ukuran;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public Sepatu(String nama, String merk, String ukuran, String stok, int harga) {
+        super(nama, stok, harga);
+        this.ukuran = ukuran;
+        this.merk = merk;
+    }
+}
+    
